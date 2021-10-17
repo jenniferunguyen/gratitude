6158dd206b3ed179a55cf45fc5643e4dcc68737c
jenniferunguyen <jenguyen@chapman.edu>
2021-10-16 21:56:48 -0700

    added type=submit to button

--- components/Input.js
+++ components/Input.js
@@ -14,7 +14,7 @@ export default function Input({handleSubmit}) {
             <input placeholder="Enter Gratitude" type="text" value={value} onChange={e => setValue(e.target.value)}
                 className="mr-2 rounded px-3 py-2 placeholder-yellow-200 text-blue-800">
             </input>
-            <button className="bg-yellow-300 hover:bg-yellow-400 rounded px-12 py-2 text-blue-800">Save</button>
+            <button type="submit" className="bg-yellow-300 hover:bg-yellow-400 rounded px-12 py-2 text-blue-800">Save</button>
         </form>
     )
 }
